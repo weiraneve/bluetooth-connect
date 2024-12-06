c022aca47a05215ac68ff225157b51626286aa7a
weiraneve <907221539@qq.com>
2024-12-06 20:42:39 +0800

    feat: add temp button

--- app/src/main/java/com/weiran/bluetooth_connect/MainActivity.kt
+++ app/src/main/java/com/weiran/bluetooth_connect/MainActivity.kt
@@ -1,47 +1,51 @@
 package com.weiran.bluetooth_connect
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.weiran.bluetooth_connect.ui.theme.BluetoothconnectTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val tag = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             BluetoothconnectTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainContent(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BluetoothconnectTheme {
-        Greeting("Android")
+    @Composable
+    fun MainContent(modifier: Modifier = Modifier) {
+        Box(
+            modifier = modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Button(
+                onClick = {
+                    // todo
+                    Log.i(tag, "Hello World")
+                }
+            ) {
+                Text(text = "按钮")
+            }
+        }
     }
 }
